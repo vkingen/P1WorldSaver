8dfcb20665e5f1c9bd0a7c67ae0a1092570f2b44
vkingen <lassevikingkristensen@gmail.com>
2021-12-01 12:15:10 +0100

    6

--- WorldSaver/Assets/Viking/Scripts/SpawnTrash.cs
+++ WorldSaver/Assets/Viking/Scripts/SpawnTrash.cs
@@ -6,21 +6,26 @@ public class SpawnTrash : MonoBehaviour
 {
     public Vector3 center;
     public Vector3 size;
+    Vector3 rotation;
 
     public GameObject[] trashPrefab;
+    public int objectsToSpawn = 50;
 
     private void Update()
     {
-        if(Input.GetKey(KeyCode.Space))
+        if (objectsToSpawn > 0)
         {
             SpawnTheTrash();
         }
     }
 
+    
+
     public void SpawnTheTrash()
     {
         Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
-
-        Instantiate(trashPrefab[Random.Range(0,trashPrefab.Length)], pos, Quaternion.identity);
+        rotation = new Vector3(0,Random.Range(0,359));
+        GameObject trashClone = Instantiate(trashPrefab[Random.Range(0, trashPrefab.Length)], pos, Quaternion.Euler(rotation));
+        objectsToSpawn--;
     }
 }
