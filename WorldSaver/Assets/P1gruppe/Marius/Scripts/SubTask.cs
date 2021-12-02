f4d33b7193ac03aaf1232dfcf285eff6c4c6c00e
Marius-Arnbjerg <marius.arnbjerg@gmail.com>
2021-12-02 13:42:39 +0100

    x

---
+++ WorldSaver/Assets/P1gruppe/Marius/Scripts/SubTask.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubTask : MonoBehaviour
+{
+    public Rigidbody spawnHarborPorpoise;
+    public Transform spawnPosition;
+    bool isSpawned = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player1" || other.tag == "Player2" && isSpawned == false)
+        {
+            Instantiate(spawnHarborPorpoise, spawnPosition.position, spawnPosition.rotation);
+            Debug.Log("Instantiated");
+
+            isSpawned = true;
+        } 
+    }
+}
