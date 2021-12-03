6071a31fbc4db4feedb643d06f23a2570965e15e
Conrad Janke <conrad@conrad.dk>
2021-12-03 14:39:27 +0100

    j
    uk

---
+++ WorldSaver/Assets/P1gruppe/Conrad/BoatSound.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatSound : MonoBehaviour
+{
+    public float audioPitch = 1;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.pitch = audioPitch;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
