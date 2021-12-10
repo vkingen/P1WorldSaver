78206bc028371289f7fd7081cb95ac5c7f357151
maltebach <malte-bach@hotmail.com>
2021-12-10 09:35:12 +0100

    s

---
+++ WorldSaver/Assets/SceneStart.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneStart : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Time.timeScale = 1f;// starter scenen igen efter at være pauset
+
+    }
+
+
+
+    
+}
