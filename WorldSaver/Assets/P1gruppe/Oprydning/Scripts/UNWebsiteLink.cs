fdcc38ec43fc46cb71d88ec45c70d68c58b634c0
Marius-Arnbjerg <marius.arnbjerg@gmail.com>
2021-12-10 12:33:41 +0100

    NoticeScene

---
+++ WorldSaver/Assets/P1gruppe/Oprydning/Scripts/UNWebsiteLink.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UNWebsiteLink : MonoBehaviour
+{
+    public void unLink()
+    {
+        Application.OpenURL("https://www.un.org/sustainabledevelopment/oceans/");
+    }
+}
