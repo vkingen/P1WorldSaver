45e6e7c40a17715b0a4f0efca2d297544d0d4195
vkingen <lassevikingkristensen@gmail.com>
2021-12-13 09:58:58 +0100

    2

--- WorldSaver/Assets/P1gruppe/Viking/Scripts/Trash.cs
+++ WorldSaver/Assets/P1gruppe/Viking/Scripts/Trash.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 
 public class Trash : MonoBehaviour
 {
-    public string player1, player2, avoidTrash;
+    [Header("Tag names")]
+    [Tooltip("Tag name: If trash spawns inside these objects, destroy this")]
+    public string player1, player2, avoidTrash; 
     private void OnTriggerEnter(Collider other)
     {
-        //if(other.tag != player1 || other.tag != player2 || other.tag != avoidTrash)
-        //{
-        //    Debug.Log( other.gameObject.name); 
-        //    Destroy(this.gameObject);
-        //    Debug.Log("Trash Removed");
-        //}
         if (other.tag == player1 || other.tag == player2 || other.tag == avoidTrash)
         {
-
         }
         else
         {
