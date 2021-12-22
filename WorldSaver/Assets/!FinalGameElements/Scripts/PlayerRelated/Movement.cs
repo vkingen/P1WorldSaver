1505312a27bd8f0638ad9d5741cf2d7aaee113f9
akaise21 <akaise21@student.aau.dk>
2021-12-22 11:54:42 +0100

    Update Movement.cs

--- WorldSaver/Assets/!FinalGameElements/Scripts/PlayerRelated/Movement.cs
+++ WorldSaver/Assets/!FinalGameElements/Scripts/PlayerRelated/Movement.cs
@@ -69,8 +69,7 @@ public class Movement : MonoBehaviour
         }
     }
 
-    private void FixedUpdate() //FixedUpdate runs x amount of times per frame
-                               //depending on framerate and how many physics frames per second are set in settings
+    private void FixedUpdate() //FixedUpdate runs 50 times per second
     {
         Vector3 rotation = Vector3.up * hInput; //Decides the rotation with a Vector 3 variable.
         Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime); //Smoothens the rotation
